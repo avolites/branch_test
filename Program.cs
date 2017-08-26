f8ae40c9d71fec4327a26b208d56f062885f75c1
Gaoxiang Liu <gaoxiangliu@outlook.com>
2017-08-26 22:29:57 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoledemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 0x12;
+            Console.WriteLine("{0:C}",a);
+        }
+    }
+}
